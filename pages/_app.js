de4eec4e57d7411546566b77414bb69afeafbc41
Kuszmar Jacek <32854834+vV1235@users.noreply.github.com>
2022-11-26 23:27:49 +0100

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,24 @@ import Head from 'next/head';
 import { CartProvider } from '@/hooks/use-shopping-cart';
 import { Header, Footer } from '@/components/index';
 import { Toaster } from 'react-hot-toast';
+import Script from "next/script";
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <Head>
         <title>
           Rarest of pepes shop
